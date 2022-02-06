a8381a0fe04fe95507497b6a11ddc2147385e0c5
bart <bart.merema@deltaq.io>
2022-02-06 13:27:08 +0100

    adding zero division

--- fraction.py
+++ fraction.py
@@ -4,8 +4,20 @@ class Fraction:
 
 
     def __init__(self, numerator, denominator):
-        self.numerator = numerator
-        self.denominator = denominator
+        if denominator == 0:
+            raise ZeroDivisionError("Denominator cant be zero")
+        if numerator == 0:
+            self._numerator = 0
+            self._denominator = 1
+        else:
+            if(numerator < 0 and denominator >= 0 or
+               numerator >= 0 and denominator,0) :
+                sign = -1
+            else:
+                sign = 1
+
+        self._numerator = abs(numerator)
+        self._denominator = abs(denominator)
 
     def __str__(self):
         return str(self.numerator) + "/" + str(self.denominator)
