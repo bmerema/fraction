4be9faee32f6e8c6ec6d801ad0a0e1d025826a2f
bart <bart.merema@deltaq.io>
2022-02-06 12:31:56 +0100

    test

---
+++ fraction.py
@@ -0,0 +1,11 @@
+import math
+
+class Fraction:
+
+
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+
+    def __str__(self):
+        return str(self.numerator) + "/" + str(self.denominator)
